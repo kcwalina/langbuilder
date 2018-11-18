71269825cf3e8e11d9f0b73280247b110383b319
Krzysztof Cwalina <kcwalina@microsoft.com>
2018-11-17 17:29:45 -0800

    added compiler

---
+++ alan/Program.cs
@@ -0,0 +1,18 @@
+﻿using langbuilder.Lexer;
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var lexer = new Lexer();
+        var tokens = lexer.GetTokens(File.ReadAllText(@".\content\demo.alan").AsMemory());
+        foreach(var token in tokens.Span) {
+            Console.WriteLine(token);
+        }
+
+        Console.ReadLine();
+    }
+}
+
