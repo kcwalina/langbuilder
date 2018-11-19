eaac24cc8f8bb4b39651d40ba14327f4264fa148
Krzysztof Cwalina <kcwalina@microsoft.com>
2018-11-18 18:11:24 -0800

    hacky but E2E works;

--- alan/Program.cs
+++ alan/Program.cs
@@ -1,6 +1,7 @@
 ﻿using langbuilder.Lexer;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 class CGenerator
@@ -27,6 +28,41 @@ class CGenerator
                 Generate(writer, function);
             }
         }
+
+        var clInfo = new ProcessStartInfo();
+        string crt = @"""c:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\ucrt""";
+        string windowsShared    = @"""c:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\shared""";
+        string windowsOther     = @"""c:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um""";
+        string vsInclude        = @"""c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include""";
+
+        clInfo.FileName = "cl.exe";
+        clInfo.Arguments = $" -c /MT /Gz /I {crt} /I {windowsShared} /I {windowsOther} /I {vsInclude} main.c";
+        clInfo.CreateNoWindow = true;
+        clInfo.WorkingDirectory = directory;
+        clInfo.RedirectStandardOutput = true;
+        clInfo.UseShellExecute = false;
+
+        Console.WriteLine($"{clInfo.FileName} {clInfo.Arguments}");
+        var clProcess = Process.Start(clInfo);
+        Console.Out.Write(clProcess.StandardOutput.ReadToEnd());
+        clProcess.WaitForExit();
+
+        string crtLibs  = @"""c:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\ucrt\x86""";
+        string vsLibs   = @"""c:\Program Files(x86)\Microsoft Visual Studio 14.0\VC\lib""";
+        string osLibs   = @"""c:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\um\x86""";
+        // /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\um\x86"
+        var linkInfo = new ProcessStartInfo();
+        linkInfo.FileName = "link.exe";
+        linkInfo.Arguments = $" /SUBSYSTEM:console main.obj /LIBPATH:{crtLibs} /LIBPATH:{vsLibs} /LIBPATH:{osLibs}";
+        linkInfo.CreateNoWindow = true;
+        linkInfo.WorkingDirectory = directory;
+        linkInfo.RedirectStandardOutput = true;
+        linkInfo.UseShellExecute = false;
+
+        Console.WriteLine($"{linkInfo.FileName} {linkInfo.Arguments}");
+        var linkProcess = Process.Start(linkInfo);
+        Console.Out.Write(clProcess.StandardOutput.ReadToEnd());
+        clProcess.WaitForExit();
     }
 
     public void Generate(TextWriter writer, FxFunction function)
@@ -86,9 +122,6 @@ class Alan
     {
         var lexer = new Lexer();
         var tokens = lexer.GetTokens(File.ReadAllText(@".\content\demo.alan").AsMemory());
-        foreach(var token in tokens.Span) {
-            Console.WriteLine(token);
-        }
 
         if(!TryParseProgram(ref tokens, out FxProgram program)) {
             Console.WriteLine("Error");
@@ -97,7 +130,6 @@ class Alan
         var generator = new CGenerator();
         
         generator.Generate(@".\src\c\", program);
-        Console.ReadLine();
     }
 
     static bool TryParseProgram(ref ReadOnlyMemory<Token> tokens, out FxProgram program)
